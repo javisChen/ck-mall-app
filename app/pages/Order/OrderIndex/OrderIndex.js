7075c836a8a441ec3f9837a748adcf2133cc97c2
chenjw@sunrizetech.com <javischen9548@foxmail.com>
2020-03-01 21:52:36 +0800

    update order  module

--- app/pages/Order/OrderIndex/OrderIndex.js
+++ app/pages/Order/OrderIndex/OrderIndex.js
@@ -5,7 +5,8 @@ import {
   Text,
   View,
   TouchableOpacity,
-  TextInput
+  Image,
+  ScrollView
 } from "react-native";
 import BaseHeader from "../../../components/BaseHeader/BaseHeader";
 import LinearGradient from 'react-native-linear-gradient'
@@ -34,15 +35,23 @@ export default class AddressIndex extends Component {
       ],
       orderList: [
         {
-          picture: '',
-          desc: '小米Redmi 256GB'
+          picture: require('@res/images/phone.jpg'),
+          desc: '小米Redmi 256GB',
+          orderId: 'A10002',
+          expense: 10.11,
+          status: 1
         }
       ]
     }
   }
-
-  save = () => {
-    this.props.navigation.navigate('AddressIndex')
+  onTabPress = (index, item) => {
+    Object.keys(this.navRefs).forEach(r => {
+      this.navRefs[index].setNativeProps({
+        style: {
+          backgroundColor: '#f3f3f3'
+        }
+      })
+    })
   };
 
   render() {
@@ -57,28 +66,65 @@ export default class AddressIndex extends Component {
             {
               this.state.tab.map((value, index) => {
                 return (
-                    <View key={index} style={styles.oi_tabItem}>
-                      <Text style={styles.oi_tabItemText}>{value.text}</Text>
-                    </View>
+                    <TouchableOpacity
+                        key={index} style={styles.oi_tabItem}
+                        onPress={this.onTabPress.bind(this, index, value)}
+                        activeOpacity={1}
+                        ref={(ref) => {
+                          return this.navRefs = {...this.navRefs, [`navRefsItem${index}`]: ref};
+                        }}>
+                      <Text style={[styles.oi_tabItemText]}>{value.text}</Text>
+                    </TouchableOpacity>
                 )
               })
             }
           </View>
 
-          {
-            this.state.orderList.map((value, index) => {
-              return (
-                  <View style={styles.oi_orderItem}>
+          <View style={styles.oi_order}>
+            <ScrollView>
+              {
+                this.state.orderList.map((value, index) => {
+                  return (
+                      <View key={index} style={styles.oi_orderItem}>
+                        <View style={styles.oi_orderItemHeader}>
+                          <Text style={styles.oi_orderId}>订单号：{value.orderId}</Text>
+                          <TouchableOpacity style={styles.oi_dustbinWrapper}>
+                            <Image
+                                style={styles.oi_dustbin}
+                                source={require('@res/images/common/dustbin.png')}
+                            />
 
-                    {/*<Image*/}
-                    {/*    style={styles.productItemImg}*/}
-                    {/*    source={require('@res/images/phone.jpg')}*/}
-                    {/*/>*/}
-                  </View>
+                          </TouchableOpacity>
+                        </View>
+                        <View style={styles.oi_orderItemContent}>
+                          <Image
+                              style={styles.oi_orderItemPic}
+                              source={require('@res/images/phone.jpg')}
+                          />
+                          <Text style={styles.oi_orderItemDesc}>{value.desc}</Text>
+                          <Text style={styles.oi_orderItemExpense}>¥
+                            <Text style={styles.oi_expenseText}>{value.expense}</Text>
+                          </Text>
+                        </View>
 
-              )
-            })
-          }
+                        <View style={styles.oi_orderItemButtonGroup}>
+                          <TouchableOpacity style={styles.oi_orderItemButton}>
+                            <Text style={styles.oi_buttonText}>查看物流</Text>
+                          </TouchableOpacity>
+                          <TouchableOpacity style={styles.oi_orderItemButton}>
+                            <Text style={styles.oi_buttonText}>查看物流</Text>
+                          </TouchableOpacity>
+                          <TouchableOpacity style={styles.oi_orderItemButton}>
+                            <Text style={styles.oi_buttonText}>查看物流</Text>
+                          </TouchableOpacity>
+                        </View>
+                      </View>
+
+                  )
+                })
+              }
+            </ScrollView>
+          </View>
 
 
         </View>
--- app/pages/Order/OrderIndex/OrderIndexStyles.js
+++ app/pages/Order/OrderIndex/OrderIndexStyles.js
@@ -24,13 +24,21 @@ export default StyleSheet.create({
     alignItems: 'center',
     width: '20%',
     height: '100%',
-    // borderWidth: 1,
+  },
+
+  oi_tabItemActived: {
+    color: CommonStyles.themeColor
+// borderWidth: 1,
     // borderColor: 'red'
   },
 
   oi_tabItemText: {
     fontSize: 13,
-    borderWidth: 1,
+    // borderWidth: 1,
+  },
+
+  oi_order: {
+    // backgroundColor: 'red'
   },
 
   oi_orderItem: {
@@ -41,6 +49,69 @@ export default StyleSheet.create({
     borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
-    height: 100,
+    padding: 10
+    // height: 40,
   },
+
+  oi_orderItemHeader: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    width: '100%',
+    // borderWidth: 1,
+  },
+  oi_orderId: {
+    fontSize: 13,
+  },
+  oi_dustbinWrapper: {
+    position: 'absolute',
+    right: 0,
+    width: 15,
+    height: 15,
+  },
+  oi_dustbin: {
+    width: 15,
+    height: 15,
+  },
+  oi_orderItemContent: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 10,
+    width: '100%'
+  },
+  oi_orderItemPic: {
+    width: 70,
+    height: 70,
+    flex: 2,
+  },
+  oi_orderItemDesc: {
+    flex: 7,
+    marginLeft: 10
+  },
+  oi_orderItemExpense: {
+    position: 'absolute',
+    right: 0,
+    fontSize: 11
+  },
+  oi_expenseText: {
+    fontWeight: '600',
+    fontSize: 16,
+  },
+  oi_orderItemButtonGroup: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    width: '100%'
+  },
+  oi_orderItemButton: {
+    borderRadius: 10,
+    borderWidth: .3,
+    padding: 5,
+    paddingLeft: 8,
+    paddingRight: 8,
+    marginLeft: 10
+  },
+  oi_buttonText: {
+    fontSize: 12
+  }
 });
